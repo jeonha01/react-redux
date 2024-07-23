446b018c0c1b02aa442c86232eb9a69f103c6bc9
Jeonha Kang <kangjeonha@gmail.com>
2024-07-23 20:49:06 +0900

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Box from './component/Box';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state) => state.count)
+  let id = useSelector(state => state.id)
+  let password = useSelector(state => state.password)
+  const dispatch = useDispatch()
   const increase = () => {
-    setCount(count + 1)
+    dispatch({ type: "INCREMENT", payload: { num: 5 } })
   }
+  const login = () => {
+    dispatch({ type: "LOGIN", payload: { id: "jeonha", password: "123" } })
+  }
+  const decrement = () => {
+    dispatch({ type: "DECREMENT", payload: { num: -5 } })
+  }
+
   return (
     <div>
+      <h1>{id},{password}</h1>
       <h1>{count}</h1>
       <button onClick={increase}>증가</button>
+      <button onClick={login}>Login</button>
+      <button onClick={decrement}>감소</button>
+      <Box />
     </div>
   );
 }
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,9 +1,22 @@
 let initialstate = {
-    count: 0
+    count: 0,
+    id: "",
+    password: "",
 }
 
 function reducer(state = initialstate, action) {
+    console.log(action)
 
+    switch (action.type) {
+        case "INCREMENT":
+            return { ...state, count: state.count + action.payload.num }
+        case "LOGIN":
+            return { ...state, id: action.payload.id, password: action.payload.password }
+        case "DECREMENT":
+            return { ...state, count: state.count + action.payload.num }
+        default:
+            return { ...state }
+    }
 }
 
 export default reducer
